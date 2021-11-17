f0808d87ea7d5807b2480e304eee80ac63cb1bf8
Jayaram Jai <jayaram@kran.co.in>
2021-11-18 00:10:39 +0530

    Day 13 - 2D Matrix
    1. Add the matrices

---
+++ src/edu/scaler/TwoDMatrix_Matrix_Addition.java
@@ -0,0 +1,64 @@
+package edu.scaler;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TwoDMatrix_Matrix_Addition {
+
+    public static void main(String[] args) {
+        TwoDMatrix_Matrix_Addition object = new TwoDMatrix_Matrix_Addition();
+
+        ArrayList<ArrayList<Integer>> A = new ArrayList<>();
+        ArrayList<Integer> a1 = new ArrayList<>(Arrays.asList(1, 2, 3));
+        ArrayList<Integer> a2 = new ArrayList<>(Arrays.asList(4, 5, 6));
+        ArrayList<Integer> a3 = new ArrayList<>(Arrays.asList(7, 8, 9));
+        A.add(a1);
+        A.add(a2);
+        A.add(a3);
+
+        ArrayList<ArrayList<Integer>> B = new ArrayList<>();
+        ArrayList<Integer> b1 = new ArrayList<>(Arrays.asList(9, 8, 7));
+        ArrayList<Integer> b2 = new ArrayList<>(Arrays.asList(6, 5, 4));
+        ArrayList<Integer> b3 = new ArrayList<>(Arrays.asList(3, 2, 1));
+        B.add(b1);
+        B.add(b2);
+        B.add(b3);
+
+        ArrayList<ArrayList<Integer>> result = object.addMatrices(A, B);
+        for (int row = 0; row < result.size(); row++) {
+            for (int col = 0; col < result.get(row).size(); col++) {
+                System.out.print(result.get(row).get(col));
+            }
+
+            System.out.println("");
+        }
+    }
+
+    /**
+     * 
+     * @param A VECTOR OF VECTOR OF INTEGERS REPRESENTING MATRIX A
+     * @param B VECOTOR OF VECTOR OF INTEGERS REPRESENTING MATRIX B
+     * @return VECTOR OF VECTOR OF INTEGERS AFTER SUM
+     * 
+     * TC : O(N^2)
+     * SC : O(N^2+N)
+     */
+    private ArrayList<ArrayList<Integer>> addMatrices(
+        ArrayList<ArrayList<Integer>> A, ArrayList<ArrayList<Integer>> B) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>(A.size());
+
+        ArrayList<Integer> temp;
+        for (int row = 0; row < A.size(); row++) {
+            temp = new ArrayList<>();
+
+            for (int col = 0; col < A.get(row).size(); col++) {
+                temp.add(A.get(row).get(col) + B.get(row).get(col));
+            }
+
+            result.add(temp);
+        }
+
+        return result;
+    }
+
+}
