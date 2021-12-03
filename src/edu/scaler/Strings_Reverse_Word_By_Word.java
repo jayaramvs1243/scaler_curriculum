293f0e93efa086cc92172e1ba8155993a437fb2c
Jayaram <jayaram@kran.co.in>
2021-12-03 22:24:23 +0530

    Day 19 - Intermediate : Strings
    1. Reverse the given sentence word by word

---
+++ src/edu/scaler/Strings_Reverse_Word_By_Word.java
@@ -0,0 +1,16 @@
+package edu.scaler;
+
+public class Strings_Reverse_Word_By_Word {
+
+    public static void main(String[] args) {
+        Strings_Reverse_Word_By_Word object = new Strings_Reverse_Word_By_Word();
+
+        String input = "MAMA";
+        System.out.println(object.reverse(input));
+    }
+
+    private String reverse(String input) {
+        return null;
+    }
+
+}
