286b46c9eeebe9e1e29b11604c7a35ee60dc9a07
Jayaram Jai <jayaram@kran.co.in>
2021-12-17 17:21:41 +0530

    Day 9 - Arrays & Dynamic Arrays
    1. Rotation

--- src/edu/scaler/Arrays_Right_Rotation.java
+++ src/edu/scaler/Arrays_Right_Rotation.java
@@ -1,44 +1,44 @@
 package edu.scaler;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
-public class Arrays_Rotation {
-
+public class Arrays_Right_Rotation {
     public static void main(String[] args) {
-        Arrays_Rotation object = new Arrays_Rotation();
+        Arrays_Right_Rotation object = new Arrays_Right_Rotation();
+        
+        Scanner input = new Scanner(System.in);
 
-        List<Integer> A = new ArrayList<>(Arrays.asList(1, 1, 1, 1, 1));
-        Integer B = 3;
+        int arrayLength = input.nextInt();
+
+        List<Integer> A = new ArrayList<>(arrayLength);
+        for(int i = 0; i < arrayLength; i++) {
+            A.add(input.nextInt());
+        }
+
+        int B = input.nextInt();
 
         if (B > A.size()) {
             B = B % A.size();
         }
 
         /* REVERSE THE ENTIRE ARRAY */
-        object.reverse(A, 0, A.size() - 1);
+        object.reverse(A, 0, arrayLength - 1);
 
         /* REVERSE SUBARRAY [0, B - 1] */
         object.reverse(A, 0, B - 1);
 
         /* REVERSE SUBARRAY [B, N] */
-        object.reverse(A, B, A.size() - 1);
+        object.reverse(A, B, arrayLength - 1);
 
-        System.out.println(A);
+        for(int i = 0; i < arrayLength; i++) {
+            System.out.print(A.get(i) + " ");
+        }
+
+        input.close();
     }
 
-    /**
-     * 
-     * @param A AN ARRAY A OF INTEGERS OF SIZE N
-     * @param start START INDEX OF THE SUBARRAY TO BE REVERSED
-     * @param end END INDEX OF THE SUBARRAY TO BE REVERSED
-     * 
-     * @return ARRAY WITH THE REVERSED SUBARRAY
-     * 
-     * TC : O(N/2)
-     * SC : O(1)
-     */
     private List<Integer> reverse(List<Integer> A, Integer start, Integer end) {
         Integer temp = 0;
 
